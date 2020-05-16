3d8e26376357cf10060063f3a1461fb17f27dd2d
luozui <329690971@qq.com>
2020-05-16 12:16:45 +0800

    add dashboard install script

--- dashboard/install_dashboard.sh
+++ dashboard/install_dashboard.sh
@@ -5,7 +5,7 @@ kubectl apply -f recommended.yaml
 
 kubectl apply -f dashboard-adminuser.yaml
 
-
+cd ~/.kube
 grep 'client-certificate-data' ~/.kube/config | head -n 1 | awk '{print $2}' | base64 -d >> kubecfg.crt
 grep 'client-key-data' ~/.kube/config | head -n 1 | awk '{print $2}' | base64 -d >> kubecfg.key
 openssl pkcs12 -export -clcerts -inkey kubecfg.key -in kubecfg.crt -out kubecfg.p12 -name "kubernetes-client"
