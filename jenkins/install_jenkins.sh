09bebb9b0618e41295812f4ea25ca37369a81451
luozui <329690971@qq.com>
2020-05-16 17:33:30 -0400

    update

--- jenkins/install_jenkins.sh
+++ jenkins/install_jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl $1 -f install.yaml
+kubectl $1 -f service-account.yml
 
 echo -e "执行以下命令露出jenkins："
 echo -e kubectl --namespace kube-logging port-forward --address 0.0.0.0 svc/jenkins 8080
